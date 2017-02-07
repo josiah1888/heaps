38e9be8073d3a44e37a10e3bd1f2abf28a5506bf
Josiah Burchard <josiah@jburchard.com>
2017-02-07 09:37:46 -0600

    MinHeap, MaxHeap

--- spec/heap.spec.ts
+++ spec/heap.spec.ts
@@ -1,8 +1,89 @@
-import { Heap } from '../src/index';
+import { MinHeap, MaxHeap } from '../src/index';
 
-describe('heap', () => {
-    it('should exist', () => {
-      const heap = new Heap()
-      expect(heap).toBeTruthy();
+describe('MinHeap', () => {
+  let minHeap: MinHeap;
+
+  beforeEach(() => {
+    minHeap = new MinHeap();
+  });
+
+
+  it('should exist', () => {
+    expect(minHeap).toBeTruthy();
+  });
+
+  describe('Peak', () => {
+    it('should throw if empty', () => {
+      expect(minHeap.peak).toThrow();
+    });
+
+    it('should show the top element', () => {
+      const x = 5;
+      minHeap.add(x);
+      expect(minHeap.peak()).toBe(x);
+    });
+
+    it('should keep the smallest value on top', () => {
+      minHeap.add(2);
+      minHeap.add(3);
+      minHeap.add(4);
+      expect(minHeap.peak()).toBe(2);
+      minHeap.add(1);
+      expect(minHeap.peak()).toBe(1);
+    });
+
+    it('should bubble the smallest values up', () => {
+      minHeap.add(10);
+      minHeap.add(15);
+      minHeap.add(20);
+      minHeap.add(17);
+      minHeap.add(25);
+      expect(minHeap.poll()).toBe(10);
+      expect(minHeap.peak()).toBe(15);
     });
   });
+});
+
+describe('MaxHeap', () => {
+  let maxHeap: MaxHeap;
+
+  beforeEach(() => {
+    maxHeap = new MaxHeap();
+  });
+
+
+  it('should exist', () => {
+    expect(maxHeap).toBeTruthy();
+  });
+
+  describe('Peak', () => {
+    it('should throw if empty', () => {
+      expect(maxHeap.peak).toThrow();
+    });
+
+    it('should show the top element', () => {
+      const x = 5;
+      maxHeap.add(x);
+      expect(maxHeap.peak()).toBe(x);
+    });
+
+    it('should keep the largest value on top', () => {
+      maxHeap.add(4);
+      maxHeap.add(3);
+      maxHeap.add(2);
+      expect(maxHeap.peak()).toBe(4);
+      maxHeap.add(6);
+      expect(maxHeap.peak()).toBe(6);
+    });
+
+    it('should bubble the largest values up', () => {
+      maxHeap.add(25);
+      maxHeap.add(17);
+      maxHeap.add(20);
+      maxHeap.add(15);
+      maxHeap.add(10);
+      expect(maxHeap.poll()).toBe(25);
+      expect(maxHeap.peak()).toBe(20);
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,119 @@
-export class Heap {
-  
+export abstract class Heap {
+  protected items: number[] = [];
+  private comparator: (a, b) => boolean;
+
+  constructor(comparator: (a, b) => boolean) {
+    this.comparator = comparator;
+  }
+
+  public peak(): number {
+    this.checkItems();
+    return this.items[0];
+  }
+
+  public poll(): number {
+    this.checkItems();
+    const item = this.items.shift();
+    this.items.unshift(this.items.pop());
+
+    this.heapifyDown();
+    return item;
+  }
+
+  public add(item: number): void {
+    this.items.push(item);
+    this.heapifyUp();
+  }
+
+  public heapifyUp(): void {
+    let index = this.items.length - 1;
+
+    while (this.comparator(this.getParent(index), this.items[index])) { // Should be > than
+      this.swap(index, this.getParentIndex(index));
+      index = this.getParentIndex(index);
+    }
+  }
+
+  public heapifyDown(): void {
+    let index = 0;
+
+    while (this.hasLeftChild(index)) {
+      let smallestChildIndex = this.getLeftChildIndex(index);
+      if (this.hasRightChild(index) && this.comparator(this.getLeftChild(index), this.getRightChild(index))) {
+        smallestChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.comparator(this.items[smallestChildIndex], this.items[index])) {
+        break;
+      }
+
+      this.swap(index, smallestChildIndex);
+      index = smallestChildIndex;
+    }
+  }
+
+  protected getLeftChildIndex(parentIndex: number): number {
+    return (2 * parentIndex) + 1;
+  }
+
+  protected getRightChildIndex(parentIndex: number): number {
+    return (2 * parentIndex) + 2;
+  }
+
+  protected getParentIndex(childIndex: number): number {
+    return Math.floor((childIndex - 1) / 2.0);
+  }
+
+  protected hasLeftChild(index: number): boolean {
+    return this.getLeftChildIndex(index) < this.items.length;
+  }
+
+  protected hasRightChild(index: number): boolean {
+    return this.getRightChildIndex(index) < this.items.length;
+  }
+
+  protected hasParent(index: number): boolean {
+    const parentIndex = this.getParentIndex(index);
+    return parentIndex < this.items.length && parentIndex !== -1;
+  }
+
+  protected getLeftChild(index: number): number {
+    return this.items[this.getLeftChildIndex(index)];
+  }
+
+  protected getRightChild(index: number): number {
+    return this.items[this.getRightChildIndex(index)];
+  }
+
+  protected getParent(index: number): number {
+    return this.items[this.getParentIndex(index)];
+  }
+
+  private checkItems(): void {
+    if (this.items.length === 0) {
+      throw new Error('Invalid Operation. Heap is Empty');
+    }
+  }
+
+  private swap(indexA: number, indexB: number): void {
+    let temp = this.items[indexA];
+    this.items[indexA] = this.items[indexB];
+    this.items[indexB] = temp;
+  }
+
+  private print(): void {
+    console.log('Printing Items as Array: ', this.items);
+  }
+}
+
+export class MinHeap extends Heap {
+  constructor() {
+    super((a, b) => a > b);
+  }
+}
+
+export class MaxHeap extends Heap {
+  constructor() {
+    super((a, b) => a < b);
+  }
 }
